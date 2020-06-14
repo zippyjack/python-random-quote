5b453ffb156a1506809a66c061d590f4693e5545
zippyjack <66913040+zippyjack@users.noreply.github.com>
2020-06-14 14:21:00 -0700

    Update get-quote.py

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,8 @@
 def main():
   
-  f = open("quotes.txt")
-  quotes = f.readlines()
-  f.close()
+ f = open("quotes.txt")
+ quotes = f.readlines()
+ f.close()
 
-  print(quotes[0])
-
-  if __name__== "__main__":
-  main()
+ print(quotes[0])
   
