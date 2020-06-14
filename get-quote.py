4b85275bcb5bc8612fbfb7bf05212820dc05b3ee
zippyjack <66913040+zippyjack@users.noreply.github.com>
2020-06-14 14:19:58 -0700

    Update get-quote.py

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,11 @@
- def main():
-  #print("Keep it logically awesome.")
-   f = open("quotes.txt")
-   quotes = f.readlines()
-   f.close()
+def main():
+  
+  f = open("quotes.txt")
+  quotes = f.readlines()
+  f.close()
 
-   print(quotes[0])
+  print(quotes[0])
 
-   if __name__== "__main__":
-   main()
+  if __name__== "__main__":
+  main()
   
